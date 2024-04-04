87adb7f3b4ef2f3c39535f76284a2515579453af
mucahit <mucahitakdeniz28@gmail.com>
2024-04-04 17:37:24 +0300

    login-register

--- app/(public)/login/page.jsx
+++ app/(public)/login/page.jsx
@@ -8,12 +8,12 @@ const login = () => {
   const handleSubmit = () => {};
   return (
     <div className="flex justify-center items-center bg-white h-screen">
-      <div className="flex justify-center items-center flex-col  bg-slate-300 w-2/4 h-1/3">
+      <div className="flex justify-center items-center flex-col  bg-slate-400 w-2/4 h-2/4 rounded-lg">
         <h2 className="text-red-900 font-bold text-2xl">Login</h2>
-        <div className="flex flex-col justify-center mt-5 mb-3 w-2/4 ">
+        <div className="flex flex-col justify-center mt-5 mb-3 w-2/4  ">
           <label htmlFor="email">Email </label>
 
-          <input 
+          <input
             name="email"
             type="text"
             placeholder=" "
@@ -32,10 +32,12 @@ const login = () => {
             required
             onChange={(e) => setPassword(e.password.value)}
           />
-        </div >
-        <button className="bg-red-900 p-2 mt-3 rounded-lg w-1/4  ">
+        </div>
+        <button className="bg-red-900 p-2 mt-3 rounded-lg w-1/4  mb-4 ">
           Sing in
         </button>
+        <a className="text-blue-800" href="/register">Don't you have an account? Sign up</a>
+
       </div>
     </div>
   );
--- app/(public)/register/page.jsx
+++ app/(public)/register/page.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 const register = () => {
-  return (
-    <div>register</div>
-  )
-}
+  const [fullName, setFullName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-export default register
+  const handleSubmit = () => {};
+  return (
+    <div className="flex justify-center items-center bg-white h-screen">
+      <div className="flex justify-center items-center flex-col  bg-slate-400 w-2/4 h-2/4 rounded-lg">
+        <h2 className="text-red-900 font-bold text-2xl">Register</h2>
+        <div className="flex flex-col justify-center mt-5 mb-2 w-2/4 ">
+          <label htmlFor="email">Full Name </label>
+
+          <input
+            name="fullName"
+            type="text"
+            placeholder=" "
+            required
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col justify-center mt-5 mb-2 w-2/4 ">
+          <label htmlFor="email">Email </label>
+
+          <input
+            name="email"
+            type="text"
+            placeholder=" "
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col justify-center mt-2 mb-2 w-2/4">
+          <label htmlFor="password">Password </label>
+
+          <input
+            name="password"
+            type="text"
+            placeholder=" "
+            required
+            onChange={(e) => setPassword(e.password.value)}
+          />
+        </div>
+        <button className="bg-red-900 p-2 mt-3 rounded-lg w-1/4 mb-3  ">
+          Register
+        </button>
+        <a className="text-blue-800" href="/login">Do you have an account? Sign in</a>
+      </div>
+    </div>
+  );
+};
+
+export default register;
